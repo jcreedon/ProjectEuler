e5d54130759f3498f7812076296818ea4826e2b6
Jacob Creedon <jcreedon@gmail.com>
2016-01-16 17:49:33 -0800

    Problems 1 through 3 complete

---
+++ euler.py
@@ -0,0 +1,50 @@
+
+
+def main():
+    print(problem_003())
+    return
+
+
+def problem_001():
+    s = 0
+    for i in range(3, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            s += i
+    return s
+
+
+def problem_002():
+    f1 = 1
+    f2 = 1
+    s = 0
+
+    while f1 < 4000000:
+        if f1 % 2 == 0:
+            s += f1
+        new_f1 = f2
+        new_f2 = f1 + f2
+        f1 = new_f1
+        f2 = new_f2
+
+    return s
+
+
+def problem_003():
+    return max(factor(600851475143))
+
+
+def factor(n):
+    i = 2
+    limit = n // 2 + 1
+    while i <= limit:
+        if n % i == 0:
+            n /= i
+            limit = n // 2 + 1
+            yield i
+        else:
+            i += 1
+    yield n
+
+
+if __name__ == "__main__":
+    main()
